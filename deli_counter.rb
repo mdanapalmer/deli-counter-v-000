db7e9c445d5e991773a46f9fccc47f19c519d986
Dana Palmer <roiai4@hotmail.com>
2018-07-12 13:25:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   #binding.pry
 end
-end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
